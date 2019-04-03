5dfeba65b5ed0f5b71e83c149aa6b9d59bb7cb46
mengyu <mengyuhs@163.com>
2019-04-03 15:28:11 +0800

    .

--- store-cloud-service-admin-api/src/main/java/cn/binux/admin/service/ContentService.java
+++ store-cloud-service-admin-api/src/main/java/cn/binux/admin/service/ContentService.java
@@ -1,10 +1,9 @@
 package cn.binux.admin.service;
 
-
 import cn.binux.admin.service.hystrix.ContentServiceHystrix;
 import cn.binux.pojo.TbCategorySecondary;
 import cn.binux.pojo.XbinResult;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,31 +17,31 @@ import java.util.Map;
  * @create 2017-04-27
  */
 
-@FeignClient(value = "xbin-store-cloud-service-admin",fallback = ContentServiceHystrix.class)
+@FeignClient(value = "xbin-store-cloud-service-admin", fallback = ContentServiceHystrix.class)
 public interface ContentService {
 
-    @RequestMapping(value = "/getCategoryList",method = RequestMethod.POST)
+    @RequestMapping(value = "/getCategoryList", method = RequestMethod.POST)
     Map<String, Object> getCategoryList(
             @RequestParam("sEcho")          Integer sEcho, 
             @RequestParam("iDisplayStart")  Integer iDisplayStart, 
             @RequestParam("iDisplayLength") Integer iDisplayLength
     );
 
-    @RequestMapping(value = "/save/category",method = RequestMethod.POST)
+    @RequestMapping(value = "/save/category", method = RequestMethod.POST)
     XbinResult saveCategory(
             @RequestParam("id")             String id, 
             @RequestParam("name")           String name, 
             @RequestParam("sort_order")     Integer sort_order
     );
 
-    @RequestMapping(value = "/getCategorySecondary",method = RequestMethod.POST)
+    @RequestMapping(value = "/getCategorySecondary", method = RequestMethod.POST)
     Map<String,Object> getCategorySecondaryList(
             @RequestParam("sEcho")          Integer sEcho, 
             @RequestParam("iDisplayStart")  Integer iDisplayStart,
             @RequestParam("iDisplayLength") Integer iDisplayLength
     );
 
-    @RequestMapping(value = "/getSearchCategorySecondaryList",method = RequestMethod.POST)
+    @RequestMapping(value = "/getSearchCategorySecondaryList", method = RequestMethod.POST)
     Map<String,Object> getSearchCategorySecondaryList(
             @RequestParam("sSearch")        String sSearch, 
             @RequestParam("sEcho")          Integer sEcho, 
@@ -50,6 +49,6 @@ public interface ContentService {
             @RequestParam("iDisplayLength") Integer iDisplayLength
     );
 
-    @RequestMapping(value = "/saveCategorySecondary",method = RequestMethod.POST)
+    @RequestMapping(value = "/saveCategorySecondary", method = RequestMethod.POST)
     XbinResult saveCategorySecondary(TbCategorySecondary categorySecondary);
 }
